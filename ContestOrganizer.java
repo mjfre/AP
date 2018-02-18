11b9c9742c9dec790a0e05f279cdca9547faf8b1
mjfre <league@WTS22.attlocal.net>
2018-02-18 12:59:55 -0800

    class stuff

--- ContestOrganizer.java
+++ ContestOrganizer.java
@@ -21,32 +21,36 @@ public class ContestOrganizer {
 	public void sort(Contestant[] arr) {
 		/* code goes here */
 	}
-	
+
 	public void createContestants() {
 		Contestant[][] newContestants = new Contestant[10][10];
-		for(int i = 0; i < 9; i++) {
-			for(int j = 0; j < 9; j++) {
-				Contestant temp = new Contestant(i,j);
+		int contestantNumber = 0;
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				Contestant temp = new Contestant(i, j);
 				temp.setScore(new Random().nextInt(200));
+				temp.setName("Person " + contestantNumber++);
 				newContestants[i][j] = temp;
 			}
 		}
 		this.contestants = newContestants;
 	}
 
-	/** Sorts each row of contestants into increasing order by score.
-	 * Postcondition: Contestant with highest score in row[k] is
-	 * 				  in the rightmost column of row[k], 0 <= k < NUM_ROWS */
-	public void sortAllRows(){
-		/* code goes here */ 
+	/**
+	 * Sorts each row of contestants into increasing order by score. Postcondition:
+	 * Contestant with highest score in row[k] is in the rightmost column of row[k],
+	 * 0 <= k < NUM_ROWS
+	 */
+	public void sortAllRows() {
+		/* code goes here */
 		Contestant temp;
-		for(Contestant[] c1 : contestants) {
-			for(int j = c1.length-2; j > 0; j--) {
-				for(int i = 0 ; i < j; i++) {
-					if(c1[i].getScore() > c1[i+1].getScore()) {
+		for (Contestant[] c1 : contestants) {
+			for (int j = c1.length - 1; j > 0; j--) {
+				for (int i = 0; i < j; i++) {
+					if (c1[i].getScore() > c1[i + 1].getScore()) {
 						temp = c1[i];
-						c1[i] = c1[i+1];
-						c1[i+1] = temp;
+						c1[i] = c1[i + 1];
+						c1[i + 1] = temp;
 					}
 				}
 			}
@@ -60,18 +64,26 @@ public class ContestOrganizer {
 	 * 
 	 * @return name of contestant with highest score.
 	 */
-	
-	public void printScores() {
-		for(Contestant[] c1 : contestants) {
-			for(Contestant c2 : c1) {
-				System.out.println(c2);
-			}
-			System.out.println();
-		}
-	}
-	public String findWinnerName() {
-		/* code goes here */
 
-		return "";
+	public void printScores() {
+		for (int i = 0; i < contestants.length; i++) {
+			for (int j = 0; j < contestants[i].length; j++) {
+				System.out.println(contestants[i][j].getName() + ": " + contestants[i][j].getScore());
+			}
+			System.out.println("\n\n");
+		}
+
+	}
+
+	public String findWinnerName() {
+		sortAllRows();
+		Contestant best = new Contestant(0, 0);
+		for (Contestant[] c1 : contestants) {
+			if (c1[c1.length - 1].getScore() > best.getScore()) {
+				best = c1[c1.length - 1];
+			}
+		}
+
+		return best.getName();
 	}
 }
--- Runner.java
+++ Runner.java
@@ -3,6 +3,8 @@ public class Runner {
 	public static void main(String[] args) {
 		ContestOrganizer co = new ContestOrganizer();
 		co.createContestants();
+		co.sortAllRows();
 		co.printScores();
+		System.out.println(co.findWinnerName());
 	}
 }
